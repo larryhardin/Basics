292536c296b9e944cb567971f6a37f5a1e17ce42
larryhardin <108157696+larryhardin@users.noreply.github.com>
2022-06-25 21:28:03 -0700

    Updated and added example of static method call. Removed module-info.java

---
+++ testJavaPushWKey/src/testJavaPushWKey/HelloWorld.java
@@ -0,0 +1,9 @@
+package testJavaPushWKey;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// Common first class example
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ testJavaPushWKey/src/testJavaPushWKey/Main.java
@@ -0,0 +1,14 @@
+package testJavaPushWKey;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Note: As the method of the class HelloWorld is static, 
+		//it does not require an instance to be executed.
+		//Furthermore, if it is instantiated and the method is called
+		//using instance reference, a warning is generated stating that
+		//a static method should be accessed in a static way
+		HelloWorld.main(args);
+	}
+
+}
